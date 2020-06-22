66208801e94846360191ee01740e2831d8f1a9df
Rahul Prajapati <rrahulp395@gmail.com>
2020-06-22 12:45:16 +0530

    Some Minor Changes
    Changed Cordinates data type from float to int. Trying to get actual cordinates(not complete).

--- ObjectDetection/Displayimg.aspx.cs
+++ ObjectDetection/Displayimg.aspx.cs
@@ -10,14 +10,14 @@ using System.Data.SqlClient;
 using System.Data;
 using System.Configuration;
 using System.IO;
-using System.Threading;
+
 
 namespace ObjectDetection
 {
     public partial class Displayimg : System.Web.UI.Page
     {
         private static DataTable mainDt = new DataTable();
-        private static int imageNo = 0;
+        private static int imageNo = 0,initH,initW,newH,newW;
         private static int totalImages=-1;
         private static String[,] cordinates = new String[20,5];
         private static int no_Of_Objects = 0;
@@ -35,11 +35,21 @@ namespace ObjectDetection
                 SqlDataAdapter sda = new SqlDataAdapter(sqlcomm);
                 sda.Fill(mainDt);
                 totalImages = mainDt.Rows.Count;
+                getInitialCordinates(mainDt.Rows[imageNo].ItemArray[2].ToString());
             }
+            
             DispImage.ImageUrl = mainDt.Rows[imageNo].ItemArray[2].ToString();
-            getCoordinates(sender, e);
+            //cropImage(sender, e);
         }
 
+        protected void getInitialCordinates(string filename)
+        {
+            string path = Path.Combine(Server.MapPath(filename));
+            System.Drawing.Image img = System.Drawing.Image.FromFile(path);
+            initH = img.Height;
+            initW = img.Width;
+        }
+        
         protected override void OnPreRender(EventArgs e)
         {
             ViewState["update"] = Session["update"];
@@ -58,36 +68,40 @@ namespace ObjectDetection
             {
                 imagePath = sdr[2].ToString();
             }
+            getInitialCordinates(imagePath);
             DispImage.ImageUrl = imagePath;
+            
+
             sqlconn.Close();
-            getCoordinates(sender, e);
+            //cropImage(sender, e);
         }
 
-        protected void getCoordinates(Object sender, EventArgs e)
-        {
-            string filePath = Path.Combine(Server.MapPath("~/data"));
-            // string cropFileName = "";
-            //string cropFilePath = "";
-            if (File.Exists(filePath))
-            {
-                System.Drawing.Image orgImg = System.Drawing.Image.FromFile(filePath);
-                Rectangle CropArea = new Rectangle(Convert.ToInt32(X.Value), Convert.ToInt32(Y.Value), Convert.ToInt32(W.Value), Convert.ToInt32(H.Value));
-                try
-                {
-                    Bitmap bitMap = new Bitmap(CropArea.Width, CropArea.Height);
-                    using (Graphics g = Graphics.FromImage(bitMap))
-                    {
-                        g.DrawImage(orgImg, new Rectangle(0, 0, bitMap.Width, bitMap.Height), CropArea, GraphicsUnit.Pixel);
-                    }
-                    //cropFileName = "crop_" + fileName;
-                    // cropFilePath = Path.Combine(Server.MapPath("~/data"), cropFileName);
-                }
-                catch (Exception ex)
-                {
-                    throw;
-                }
-            }
-        }
+        //protected void cropImage(Object sender, EventArgs e)
+        //{
+        //    string filename = mainDt.Rows[imageNo].ItemArray[1].ToString();
+        //    string filePath = Path.Combine(Server.MapPath("~/data"),filename);
+        //    // string cropFileName = "";
+        //    //string cropFilePath = "";
+        //    if (File.Exists(filePath))
+        //    {
+        //        System.Drawing.Image orgImg = System.Drawing.Image.FromFile(filePath);
+        //        Rectangle CropArea = new Rectangle(Convert.ToInt32(X.Value), Convert.ToInt32(Y.Value), Convert.ToInt32(W.Value), Convert.ToInt32(H.Value));
+        //        try
+        //        {
+        //            Bitmap bitMap = new Bitmap(CropArea.Width, CropArea.Height);
+        //            using (Graphics g = Graphics.FromImage(bitMap))
+        //            {
+        //                g.DrawImage(orgImg, new Rectangle(0, 0, bitMap.Width, bitMap.Height), CropArea, GraphicsUnit.Pixel);
+        //            }
+        //            //cropFileName = "crop_" + fileName;
+        //            // cropFilePath = Path.Combine(Server.MapPath("~/data"), cropFileName);
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            throw;
+        //        }
+        //    }
+        //}
 
         protected void Load_Previous_Image(object sender,EventArgs e)
         {
@@ -101,6 +115,7 @@ namespace ObjectDetection
                 if (imageNo > 0)
                 {
                     imageNo--;
+                    getInitialCordinates(mainDt.Rows[imageNo].ItemArray[2].ToString());
                     DispImage.ImageUrl = mainDt.Rows[imageNo].ItemArray[2].ToString();
                 }
                 else
@@ -114,6 +129,7 @@ namespace ObjectDetection
 
         protected void Load_Next_Image(object sender, EventArgs e)
         {
+            
             if (Session["update"].ToString() == ViewState["update"].ToString())
             {
                 if (no_Of_Objects > 0)
@@ -125,6 +141,7 @@ namespace ObjectDetection
                 {
 
                     imageNo = imageNo + 1;
+                    getInitialCordinates(mainDt.Rows[imageNo].ItemArray[2].ToString());
                     DispImage.ImageUrl = mainDt.Rows[imageNo].ItemArray[2].ToString();
                 }
                 else
@@ -145,24 +162,28 @@ namespace ObjectDetection
                 
                 if(no_Of_Objects > 0)
                 {
+                    
                     int id = (Int32)mainDt.Rows[imageNo].ItemArray[0];
+                    
                     SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Admin\Desktop\ObjectDetection\Images.mdf;Integrated Security=True;Connect Timeout=30");
                     con.Open();
+
+                    // To add Data of all the objects into database...
                     for (int i = 0; i < no_Of_Objects; i++)
                     {
                         SqlCommand cmd = new SqlCommand("INSERT INTO Coordinates ([ImgId],[X-Dimension],[Y-Dimension],[Height],[Width],[Label]) VALUES(@Id,@X,@Y,@H,@W,@Label)", con);
 
                         cmd.Parameters.Add("@Id", System.Data.SqlDbType.Int);
-                        cmd.Parameters.Add("@X", System.Data.SqlDbType.Float);
-                        cmd.Parameters.Add("@Y", System.Data.SqlDbType.Float);
-                        cmd.Parameters.Add("@H", System.Data.SqlDbType.Float);
-                        cmd.Parameters.Add("@W", System.Data.SqlDbType.Float);
+                        cmd.Parameters.Add("@X", System.Data.SqlDbType.Int);
+                        cmd.Parameters.Add("@Y", System.Data.SqlDbType.Int);
+                        cmd.Parameters.Add("@H", System.Data.SqlDbType.Int);
+                        cmd.Parameters.Add("@W", System.Data.SqlDbType.Int);
 
                         cmd.Parameters["@Id"].Value = id;
-                        cmd.Parameters["@X"].Value = float.Parse(cordinates[i, 0]);
-                        cmd.Parameters["@Y"].Value = float.Parse(cordinates[i, 1]);
-                        cmd.Parameters["@H"].Value = float.Parse(cordinates[i, 2]);
-                        cmd.Parameters["@W"].Value = float.Parse(cordinates[i, 3]);
+                        cmd.Parameters["@X"].Value = int.Parse(cordinates[i, 0]);
+                        cmd.Parameters["@Y"].Value = int.Parse(cordinates[i, 1]);
+                        cmd.Parameters["@H"].Value = int.Parse(cordinates[i, 2]);
+                        cmd.Parameters["@W"].Value = int.Parse(cordinates[i, 3]);
                         cmd.Parameters.AddWithValue("@Label", cordinates[i, 4]);
                         cmd.ExecuteNonQuery();
                     }
